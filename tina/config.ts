9c717a2c359a9aba2e6c30dc3250b244427a2816
davidbaillydev <david.bailly8@gmail.com>
2024-02-24 19:16:10 +0100

    ok

--- tina/config.ts
+++ tina/config.ts
@@ -8,7 +8,7 @@ const branch =
   "main";
 
 export default defineConfig({
-  branch : "main",
+  branch : "master",
 
   // Get this from tina.io
   clientId: "6597cfc6-ab64-4732-9a63-16d64864d846",
